6ad211c06ec9b8eee0b06b5203ec594754a43b27
name-is-jayant <jayant@antino.com>
2025-03-01 19:19:13 +0530

    feat : added icons and colors

---
+++ src/screens/scoreboard/components/ScoreboardRow.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from 'react';
+
+type Props = {
+  search?: ReactNode;
+  selectSport?: ReactNode;
+  list?: ReactNode;
+};
+
+const Scoreboard: React.FC<Props> = ({search, selectSport, list}) => {
+  return <></>;
+};
